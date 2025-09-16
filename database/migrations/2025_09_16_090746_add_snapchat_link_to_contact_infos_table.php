5278362b2611e7cb60a987d99ddac91c0544a722
ahmedhabibwork <ahmedhabibwork80@gmail.com>
2025-09-16 13:21:43 +0300

    hot fix change

--- database/migrations/2025_09_16_090746_add_snapchat_link_to_contact_infos_table.php
+++ database/migrations/2025_09_16_090746_add_snapchat_link_to_contact_infos_table.php
@@ -12,8 +12,8 @@ return new class extends Migration
     public function up(): void
     {
         Schema::table('contact_infos', function (Blueprint $table) {
-                   $table->string('snapchat_link')->nullable()->after('twitter_link'); 
-
+            $table->string('snapchat_link')->nullable()->after('facebook_link');
+            
         });
     }
 
