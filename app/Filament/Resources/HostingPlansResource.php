b0f2f12e7add247ad3e7417c23d8320c64140b30
ahmedhabibwork <ahmedhabibwork80@gmail.com>
2025-09-23 14:46:23 +0300

    Finsh

--- app/Filament/Resources/HostingPlansResource.php
+++ app/Filament/Resources/HostingPlansResource.php
@@ -138,7 +138,7 @@ class HostingPlansResource extends Resource
             ])
             ->actions([
                 Tables\Actions\EditAction::make(),
-                //  Tables\Actions\DeleteAction::make(),
+                Tables\Actions\DeleteAction::make(),
             ])
             ->bulkActions([
                 Tables\Actions\DeleteBulkAction::make(),
